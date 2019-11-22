bfb7bd92f4b023280e6e9be4e614c7291aa09ac7
John Cabia-an <cabia-anjo_sd2053@ceaccs.net>
2019-11-22 17:36:58 +0800

    adding router guard

--- pnsider/src/router/index.js
+++ pnsider/src/router/index.js
@@ -1,28 +1,46 @@
 import Vue from "vue";
+import store from "@/store/index.js"
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
-  {
-    path: "/Dashboard",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
-];
-
 const router = new VueRouter({
-  routes
-});
+  mode: 'history',
+  routes:
+  [
+    {
+      path: "/",
+      name: "home",
+      component: Home
+    },
+    {
+      path: "/Dashboard",
+      name: "about",
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  
+        meta: {
+          requiresAuth: true
+        }
+    }
+  ]
+}) 
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    if (store.getters.isLoggedIn) {
+      next()
+      return
+    }
+    next('/')
+  } else {
+    next()
+  }
+})
+
 
 export default router;
